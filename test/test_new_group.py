1981118cfd2afba9f1ad57a5772ebfa8feb8b5aa
mossymoon <stephan.sergienko@gmail.com>
2022-04-27 18:21:46 +0300

    new_class_for_session_control

--- test/test_new_group.py
+++ test/test_new_group.py
@@ -12,10 +12,10 @@ def app(request):
 
 def test_add_group(app):
     app.session.login("admin")
-    app.create_group(Group(name="test", header="test", footer="test"))
+    app.group.create(Group(name="test", header="test", footer="test"))
     app.session.logout()
 
 def test_empty_group(app):
     app.session.login("admin")
-    app.create_group(Group(name="", header="", footer=""))
+    app.group.create(Group(name="", header="", footer=""))
     app.session.logout()
