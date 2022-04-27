ea09dea88051fe6a27c0049dc242c4191d6f03a5
mossymoon <stephan.sergienko@gmail.com>
2022-04-27 15:31:58 +0300

    fixture_update_file_with_separate_class

---
+++ test_new_group.py
@@ -0,0 +1,21 @@
+# тест из задания №1 (1 урок): с добавленными методами и классами
+
+import pytest
+from group import Group
+from application import Application
+
+@pytest.fixture()
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.login("admin")
+    app.create_group(Group(name="test", header="test", footer="test"))
+    app.logout()
+
+def test_empty_group(app):
+    app.login("admin")
+    app.create_group(Group(name="", header="", footer=""))
+    app.logout()
