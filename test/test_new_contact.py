81cd3dc3319e678158e879a824453d46a584b1db
mossymoon <stephan.sergienko@gmail.com>
2022-04-27 21:04:08 +0300

    issue_with_creation_contact

--- test/test_new_contact.py
+++ test/test_new_contact.py
@@ -1,26 +1,26 @@
 # Задание №4 создание фикстуры с вынесением отдельного класса
 import pytest
 from model.contact import Contact
-from fixture.new_contact import New_Contact
+from fixture.application_contact import Contact_Else
 
 @pytest.fixture()
 def app(request):
-    fixture = New_Contact()
+    fixture = Contact_Else()
     request.addfinalizer(fixture.destroy)
     return fixture
 
 def test_case_contact(app):
     app.open_home_page()
-    app.login("admin", "secret")
-    app.new_contact()
+    app.session.login("admin", "secret")
+    # app.new_contact()
     app.create_new_contact(Contact(firstname="Ivan", lastname="Ivanov", address="Moscow", mobile=3232323))
-    app.return_to_homepage()
-    app.logout()
+    # app.return_to_homepage()
+    app.session.logout()
 
 def test_empty_case_contact(app):
-    app.login("admin", "secret")
+    app.session.login("admin", "secret")
     app.create_new_contact(Contact(firstname="", lastname="", address="", mobile=""))
-    app.logout()
+    app.session.logout()
 
 def tearDown(self):
     self.app.destroy()
