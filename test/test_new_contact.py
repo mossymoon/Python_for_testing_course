77f1825503b7bc98a50e0a1e6d12765e9af119da
stepan <stephan.sergienko@gmail.com>
2022-05-17 18:53:06 +0300

    коммит связанный с ошибкой "assert (1 + 1) == 1"

--- test/test_new_contact.py
+++ test/test_new_contact.py
@@ -1,9 +1,9 @@
 from model.contact import Contact
 
 def test_case_contact(app):
-    old_contacts = app.contact.get_contact_list
+    old_contacts = app.contact.get_contact_list()
     app.contact.create_new_contact(Contact(firstname="Ivan2", lastname="Ivanov3", address="Moscow4", mobile=3232323))
-    new_contacts = app.contact.get_contact_list
+    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
 
 # def test_empty_case_contact(app):
