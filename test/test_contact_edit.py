8680d9184e31baa9cc153eebf37b5d1265389d97
mossymoon <stephan.sergienko@gmail.com>
2022-05-18 22:12:44 +0300

    commit renew the tests without edit_py

--- test/test_contact_edit.py
+++
@@ -1,6 +0,0 @@
-from model.contact import Contact
-
-def test_edit_first_contact(app):
-    if app.contact.count() == 0:
-        app.contact.create_new_contact(Contact(firstname="Ivanovich"))
-    app.contact.test_edit_first_contact(Contact(firstname="New firstname"))
